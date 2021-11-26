ac74933f2613c4fe928e4aff28d9e29c547b13ee
UP2018781 <91746177+UP2018781@users.noreply.github.com>
2021-11-26 19:56:20 +0000

    opening the box

--- Public/index.js
+++ Public/index.js
@@ -1,7 +1,42 @@
+const route = '127.0.0.1:8080';
+
+function createButton(height,width,text,id=""){
+    const h = `${parseInt(height)}px`;
+    const w = `${parseInt(width)}px`;
+    const t = `${text.toString()}`;
+    const newButton = document.createElement("button");
+
+    newButton.style.height = h;
+    newButton.style.width = w;
+    newButton.textContent = t;
+    newButton.id = id;
+    return(newButton);
+}
+
+
 function ariName(){
     const name = document.createElement('div');
     name.textContent = 'ari argyropoulou';
     console.log(name);
     document.body.appendChild(name);
 }
-ariName();
+
+async function simpleFetchText(){
+    const response = await fetch('/text');
+    const data = await response.text();
+    return(data);
+}
+
+async function openBox(e){
+    e.target.textContent = (await simpleFetchText()).toString();
+}
+
+function main(){
+    
+    document.body.append(createButton(100,100,"Aris Box","arisBox"));
+    const mainButton = document.querySelector('#arisBox');
+    mainButton.addEventListener("click",(e)=>{openBox(e)});
+    
+}
+
+main();
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+async function returnText(req,res){
+    res.send('cum');
+}
+
 app.use(express.static('public'));
+app.get('/text',returnText)
 
 app.listen(port,()=>{console.log(`server listening on port ${port}`)})
