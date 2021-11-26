d87869144ab47ade23900e14a40cf8721e625296
UP2018781 <91746177+UP2018781@users.noreply.github.com>
2021-11-26 19:14:40 +0000

    basic server functions

---
+++ Public/index.js
@@ -0,0 +1,7 @@
+function ariName(){
+    const name = document.createElement('div');
+    name.textContent = 'ari argyropoulou';
+    console.log(name);
+    document.body.appendChild(name);
+}
+ariName();
